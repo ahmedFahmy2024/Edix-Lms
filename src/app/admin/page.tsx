105d9e72205b97d2c297e78fc5342ab3e29c048d
Ahmed Fahmy <ahmedmewans2030@gmail.com>
2025-10-27 19:38:26 +0300

    feat(finished analytic)

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -3,6 +3,14 @@ import { DataTable } from "@/components/sidebar/data-table";
 import { SectionCards } from "@/components/sidebar/section-cards";
 
 import data from "./data.json";
+import Link from "next/link";
+import { buttonVariants } from "@/components/ui/button";
+import { adminGetRecentCourses } from "../data/admin/admiin-get-recent-courses";
+import EmptyState from "@/components/general/EmptyState";
+import AdminCourseCard, {
+  AdminCourseCardSkeleton,
+} from "./courses/_components/AdminCourseCard";
+import { Suspense } from "react";
 
 export default function AdminIndexPage() {
   return (
@@ -12,6 +20,55 @@ export default function AdminIndexPage() {
         <ChartAreaInteractive />
       </div>
       <DataTable data={data} />
+
+      <div className="px-4 lg:px-6 space-y-4 mt-6">
+        <div className="flex items-center justify-between">
+          <h2 className="text-xl font-semibold">Recent Courses</h2>
+          <Link
+            href="/admin/courses"
+            className={buttonVariants({ variant: "outline" })}
+          >
+            View all courses
+          </Link>
+        </div>
+
+        <Suspense fallback={<CoursesSkeleton />}>
+          <RenderRecentCourses />
+        </Suspense>
+      </div>
     </>
+  );
+}
+
+async function RenderRecentCourses() {
+  const courses = await adminGetRecentCourses();
+
+  if (courses.length === 0) {
+    return (
+      <EmptyState
+        title="No courses found"
+        description="No courses found"
+        buttonLabel="create course"
+        buttonLink="/admin/courses/create"
+      />
+    );
+  }
+
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+      {courses.map((course) => (
+        <AdminCourseCard key={course.id} data={course} />
+      ))}
+    </div>
+  );
+}
+
+function CoursesSkeleton() {
+  return (
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-1 lg:grid-cols-2 gap-7">
+      {Array.from({ length: 4 }).map((_, i) => (
+        <AdminCourseCardSkeleton key={i} />
+      ))}
+    </div>
   );
 }
--- src/app/data/admin/admiin-get-recent-courses.ts
+++ src/app/data/admin/admiin-get-recent-courses.ts
@@ -3,27 +3,26 @@ import "server-only";
 import { prisma } from "@/lib/db";
 import { requireAdmin } from "./require-admin";
 
-export async function adminGetCourses() {
+export async function adminGetRecentCourses() {
   await requireAdmin();
 
   const data = await prisma.course.findMany({
     orderBy: {
       createdAt: "desc",
     },
+    take: 2,
     select: {
       id: true,
       title: true,
       smallDescription: true,
       duration: true,
-      fileKey: true,
       level: true,
-      price: true,
-      slug: true,
       status: true,
+      price: true,
+      fileKey: true,
+      slug: true,
     },
   });
 
   return data;
 }
-
-export type AdminCourseType = Awaited<ReturnType<typeof adminGetCourses>>[0];
